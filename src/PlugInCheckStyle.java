8a921278ac6c57741c4b7b132b6c11d80cdadf96
Khizar <Khizar@Khizar-PC>
2016-05-30 17:15:39 +0500

    repository_file

---
+++ src/PlugInCheckStyle.java
@@ -0,0 +1,18 @@
+
+public class PlugInCheckStyle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		dingi d=new dingi();
+	int sub=	d.subtract();
+	System.out.print("the answer is: "+sub);
+	sum();}
+	
+	static void sum(){
+		final int a=13,b=10;
+	int c;
+		c=a+b;
+		System.out.print("the sum is: "+c);
+		}	
+
+}
---
+++ src/dingi.java
@@ -0,0 +1,10 @@
+
+public class dingi {
+	private int num1=39,num2=19;
+	
+	public int  subtract()
+	{
+		return num1=num2;
+	}
+
+}
